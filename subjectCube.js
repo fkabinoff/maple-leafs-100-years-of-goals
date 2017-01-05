dd000ebe0f782d45555196804d7710693c97db7c
Francis Kabinoff <francis.kabinoff@gmail.com>
2017-01-05 11:07:14 -0500

    changeMeasure capability

--- subjectCube.js
+++ subjectCube.js
@@ -1,4 +1,6 @@
 let subjectCube = {};
+let playerMeasure = "Sum(Goals) + Sum([Post-season Goals])";
+let opponentMeasure = "Sum({<[Regular/Post Season]={'Regular season'}>} [Opponent Goals]) + Sum({<[Regular/Post Season]={'Post-season'}>} [Opponent Goals])";
 
 subjectCube.init = () => {
   return app.createSessionObject({
@@ -51,7 +53,7 @@ subjectCube.changeState = (state) => {
       {
         qOp: "replace",
         qPath: "/qHyperCubeDef/qMeasures/0/qDef/qDef",
-        qValue: JSON.stringify("Sum(Goals) + Sum([Post-season Goals])")
+        qValue: JSON.stringify(playerMeasure)
       }
     ]);
   } else if (state === "OpponentState") {
@@ -73,6 +75,27 @@ subjectCube.changeState = (state) => {
       }
     ]);
   }
+}
+
+subjectCube.changeMeasure = (measure) => {
+  if (measure === "Total") {
+    playerMeasure = "Sum(Goals) + Sum([Post-season Goals])";
+  } else if (measure === "EV") {
+    playerMeasure = "Sum([Even Strength Goals]) + Sum([Post-season Even Strength Goals])";
+  } else if (measure === "PP") {
+    playerMeasure = "Sum([Power Play Goals]) + Sum([Post-season Power Play Goals])";
+  } else if (measure === "SH") {
+    playerMeasure = "Sum([Short Handed Goals]) + Sum([Post-season Short Handed Goals])";
+  } else if (measure === "GW") {
+    playerMeasure = "Sum(Game Winning Goals) + Sum([Post-season Game Winning Goals])";
+  }
+  return subjectCube.object.applyPatches([
+    {
+      qOp: "replace",
+      qPath: "/qHyperCubeDef/qMeasures/0/qDef/qDef",
+      qValue: JSON.stringify(playerMeasure)
+    }
+  ]);
 }
 
 export default subjectCube;
