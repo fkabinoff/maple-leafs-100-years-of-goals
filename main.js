d8a6c69cef14e08607186f5b1ea1264fe050c2b7
Francis Kabinoff <francis.kabinoff@gmail.com>
2017-01-19 15:20:47 -0500

    begin work on season chart

--- main.js
+++ main.js
@@ -2,7 +2,6 @@ import qlikapp from "./qlikapp";
 import qObjects from "./qObjects";
 
 qObjects.then((qObjects) => {
-  console.log(qObjects);
   $("input[type=radio][name=state]").change((event) => {
     let state = event.currentTarget.value;
     qObjects.forEach((qObject) => {
--- seasonCube.js
+++ seasonCube.js
@@ -1,4 +1,5 @@
 import qlikapp from "./qlikapp";
+import * as d3 from "d3";
 
 let seasonCube = {};
 
@@ -59,8 +60,23 @@ seasonCube.init = () => {
     });
   }).then((object) => {
     seasonCube.object = object;
+
+    let margin = {top: 0, right: 0, bottom: 30, left: 40},
+        width = 500 - margin.left - margin.right,
+        height = 100 - margin.top - margin.bottom,
+        svg = d3.select("#season-chart").append("svg")
+          .attr("viewbox", "0 0 500 1000")
+          .attr("preserveAspectRatio", "xMinYMin meet")
+          .attr("width", "100%")
+          .attr("height", "100%")
+        .append("g")
+          .attr("transform", "translate(" + margin.left + "," + margin.top + ")");
+
     const update = () => object.getLayout().then((layout) => {
-      //todo
+      let x = d3.scaleBand().rangeRound([0, width]).padding(0.1),
+          y = d3.scaleLinear().rangeRound([height, 0]);
+      x.domain(layout.qHyperCube.qDataPages[0].qMatrix.length);
+      y.domain([0, Math.max(...layout.qHyperCube.qDataPages[0].qMatrix.map((year) => { return year[1].qNum + year[2].qNum }))]);
     });
     object.on('changed', update);
     update();
